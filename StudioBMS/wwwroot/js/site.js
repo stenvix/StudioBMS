4aebcac93aca6c7942036530574c409925f478eb
Stepanov Valentin <falken.ua@gmail.com>
2017-05-30 15:18:17 +0300

    Add charts to statistics module

--- StudioBMS/wwwroot/js/site.js
+++ StudioBMS/wwwroot/js/site.js
@@ -1,14 +1,194 @@
 ﻿// Write your Javascript code.
 
-$(document).ready(function() {
-    $(".chosen").chosen({
-        inherit_select_classes: true,
-        disable_search_threshold: 10,
-        display_disabled_options: false,
-        display_selected_options: false,
-        max_selected_options: 4,
-        no_results_text: "Оопс, нічого не знайдено!",
-        placeholder_text_multiple: "Виберіть декілька позицій",
-        placeholder_text_single: "Виберіть дані"
+window.chartColors = {
+    red: 'rgb(255, 99, 132)',
+    orange: 'rgb(255, 159, 64)',
+    yellow: 'rgb(255, 205, 86)',
+    green: 'rgb(75, 192, 192)',
+    blue: 'rgb(54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rgb(201, 203, 207)'
+};
+
+$(document).ready(function () {
+    var items = $(".chosen");
+    items.each(function(i, e) {
+        var selected_options = $(e).data("selmax");
+
+        if (!selected_options) {
+            selected_options = 0;
+        }
+
+        $(e).chosen({
+            inherit_select_classes: true,
+            disable_search_threshold: 10,
+            display_disabled_options: false,
+            display_selected_options: false,
+            max_selected_options: selected_options,
+            no_results_text: "Оопс, нічого не знайдено!",
+            placeholder_text_multiple: "Виберіть декілька позицій",
+            placeholder_text_single: "Виберіть дані"
+        });
     });
+
+
+    var ctx = document.getElementById("myChart").getContext('2d');
+    makeChart(ctx);
+    ctx = document.getElementById("myChart1").getContext('2d');
+    makePieChart(ctx);
+    ctx = document.getElementById("myChart2").getContext('2d');
+    makeLineChart(ctx);
+    ctx = document.getElementById("myChart3").getContext('2d');
+    makeChart(ctx);
+    function makeChart(ctx) {
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    }
+    function randomScalingFactor() {
+        return Math.round(Math.random() * 100);
+    };
+
+    function makePieChart(ctx) {
+
+        var config = {
+            type: 'pie',
+            data: {
+                datasets: [{
+                    data: [
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                    ],
+                    backgroundColor: [
+                        window.chartColors.red,
+                        window.chartColors.orange,
+                        window.chartColors.yellow,
+                        window.chartColors.green,
+                        window.chartColors.blue,
+                    ],
+                    label: 'Dataset 1'
+                }],
+                labels: [
+                    "Red",
+                    "Orange",
+                    "Yellow",
+                    "Green",
+                    "Blue"
+                ]
+            },
+            options: {
+                responsive: true
+            }
+        };
+
+        var skills = new Chart(ctx, config);
+    }
+
+    function makeLineChart(ctx) {
+        var MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        var config = {
+            type: 'line',
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [{
+                    label: "My First dataset",
+                    backgroundColor: window.chartColors.red,
+                    borderColor: window.chartColors.red,
+                    data: [
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor()
+                    ],
+                    fill: false,
+                }, {
+                    label: "My Second dataset",
+                    fill: false,
+                    backgroundColor: window.chartColors.blue,
+                    borderColor: window.chartColors.blue,
+                    data: [
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor()
+                    ],
+                }]
+            },
+            options: {
+                responsive: true,
+                title: {
+                    display: true,
+                    text: 'Chart.js Line Chart'
+                },
+                tooltips: {
+                    mode: 'index',
+                    intersect: false,
+                },
+                hover: {
+                    mode: 'nearest',
+                    intersect: true
+                },
+                scales: {
+                    xAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Month'
+                        }
+                    }],
+                    yAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Value'
+                        }
+                    }]
+                }
+            }
+        };
+
+        var line = new Chart(ctx, config);
+    }
 })
