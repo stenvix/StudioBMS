852fc2af3a69d74ef6c306f9669d74aa7d071ace
Stepanov Valentin <falken.ua@gmail.com>
2017-06-05 22:28:29 +0300

    prepare email service

---
+++ StudioBMS/Services/Impl/EmailSender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace StudioBMS.Services.Impl
+{
+    public class EmailSender: IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
